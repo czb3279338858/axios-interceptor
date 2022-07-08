eed8b0fe229f677210f7323e74b29ff09c05a495
蔡朝勃 <3279338858@qq.com>
2022-07-08 10:00:56 +0800

    增加说明

--- src/request/axiosUpdateUser.ts
+++ src/request/axiosUpdateUser.ts
@@ -10,6 +10,7 @@ export const axiosUpdateUser = async (params: {
   weixin?: string
 }) => {
   return await selfAxios.post<Response<null>>('/ucenterapi/internalMtdsWeb/user/updateOrganizationalUserBasic', params, {
+    /** 当前接口返回成功时，url==='/ucenterapi/uc/internal/common/getCurrentUser'接口的缓存将会被清空 */
     _cleanMatchFun: (config) => {
       return config.url === '/ucenterapi/uc/internal/common/getCurrentUser'
     }
