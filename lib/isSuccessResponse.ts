47c88da2283c294255a9b522a667deb939d842e8
蔡朝勃 <3279338858@qq.com>
2024-02-03 08:05:02 +0800

    1

--- lib/isSuccessResponse.ts
+++
@@ -1,5 +0,0 @@
-import { AxiosResponse } from "axios";
-export function isSuccessResponse(value: AxiosResponse) {
-  if (value.status >= 200 && value.status < 300) return true
-  return false
-}
--- lib/main.ts
+++
@@ -1,5 +0,0 @@
-export { useCacheInterceptor } from './cache'
-export { getKey } from './getKey'
-export { useDebounceInterceptor } from './debounce'
-export { useTimestampInterceptor } from './timestamp'
-export { useAwaitInterceptor } from './await'
