e58bb1a3f38eaf29e947a6a4b8b1740d47352a5b
蔡朝勃 <3279338858@qq.com>
2024-02-03 08:41:12 +0800

    1

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import dtsPlugin from 'vite-plugin-dts'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: './lib/main.ts',
+      name: 'AxiosInterceptor',
+      fileName: 'axios-interceptor'
+    }
+  },
+  plugins: [dtsPlugin({
+    rollupTypes: true
+  })]
+})
